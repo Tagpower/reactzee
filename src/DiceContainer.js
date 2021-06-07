48dcc0f93eb92db826cc657f1bd8622434605518
Clément Bauchet <clem.ssbm@hotmail.fr>
2021-06-08 01:11:46 +0200

    Add everything

---
+++ src/DiceContainer.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import Die from "./Die";
+import "./DiceContainer.css"
+import RollButton from './RollButton';
+import ScoreContainer from './ScoreContainer'
+
+export default class DiceContainer extends React.Component {
+
+	state = {
+		roll: 0,
+		numbers: [1,2,3,4,5],
+		hold: [false, false, false, false, false],
+		tableClicked: false,
+		gameOver: true,
+		actionText: "Commencer la partie",
+		scoreContainerKey: Math.random().toString(36).substring(7) //Reset the scoreboard when resetting the state
+	}
+
+	toggleDieHold = (id) => {
+		if (this.state.roll !== 0 && this.state.roll < 3 && !this.state.tableClicked) {
+			let holds = this.state.hold
+			holds[id] = !holds[id]
+			this.setState({hold: holds});
+		}
+	}
+
+	handleRollClick = () => {
+		if (this.state.gameOver) {
+			this.newGame();
+		} else if (this.state.roll === 3 && this.state.tableClicked) {
+			this.newTurn();
+		} else if (this.state.roll !== 3) {
+			this.newRoll();
+		}
+	}
+
+	newGame = () => {
+		this.setState({
+			roll: 0,
+			numbers: [1,1,1,1,1],
+			hold: [false, false, false, false, false],
+			rollClicked: true,
+			tableClicked: false,
+			gameOver: false,
+			scoreContainerKey: Math.random().toString(36).substring(7)},
+			() => {this.newTurn()}
+		)
+	}
+
+	newTurn = () => {
+		this.setState({
+			roll: 0,
+			hold: [false, false, false, false, false],
+			rollClicked: true,
+			tableClicked: false,
+			gameOver: false},
+			() => {this.newRoll()}
+		)
+	}
+
+	newRoll = () => {
+		this.rollDice()
+		this.setState({roll: this.state.roll+1, actionText: "Lancer " + (this.state.roll+1)},
+			() => {if (this.state.roll === 3) {
+				this.setState({actionText: "Choisissez une ligne", tableClicked:false, hold: [false, false, false, false, false]});
+			}
+			console.log(this.state.tableClicked);
+		});
+
+	}
+
+	rollDice = () => {
+		let newNumbers = [...this.state.numbers];
+		for (var i=0; i<5; i++) {
+			if (!this.state.hold[i]) {
+				newNumbers[i] = Math.floor(Math.random()*6)+1;
+			}
+		}
+		this.setState({numbers: newNumbers});
+	}
+
+	handleScoreClick = () => {
+		this.setState({
+			tableClicked:true,
+			hold: [false, false, false, false, false],
+			actionText: "Relancez les dés"
+		});
+	}
+
+	handleGameOver = (finalScore) => {
+		this.setState({
+			gameOver: true,
+			actionText: "Partie terminée ! Score total : " + finalScore
+		});
+		console.log(finalScore);
+	}
+
+	render() {
+		return (
+		<>
+		<div className="dice-container">
+			<div className="dice-bar">
+				<Die id={0} number={this.state.numbers[0]} hold={this.state.hold[0]} toggleDieHold={this.toggleDieHold} />
+				<Die id={1} number={this.state.numbers[1]} hold={this.state.hold[1]} toggleDieHold={this.toggleDieHold} />
+				<Die id={2} number={this.state.numbers[2]} hold={this.state.hold[2]} toggleDieHold={this.toggleDieHold} />
+				<Die id={3} number={this.state.numbers[3]} hold={this.state.hold[3]} toggleDieHold={this.toggleDieHold} />
+				<Die id={4} number={this.state.numbers[4]} hold={this.state.hold[4]} toggleDieHold={this.toggleDieHold} />
+			</div>
+		</div>
+		<p>{this.state.actionText}</p>
+
+		<RollButton gameState={this.state} handleRollClick={this.handleRollClick} />		
+
+      	<ScoreContainer gameState={this.state} handleScoreClick={this.handleScoreClick} handleGameOver={this.handleGameOver} key={this.state.scoreContainerKey} />
+
+		</>)
+	}
+}
---
+++ src/ScoreRow.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import "./ScoreRow.css"
+
+export default class ScoreRow extends React.Component {
+
+
+	state = {
+		used: false,
+		score: 0,
+		scoreDisplayed: ""
+	}
+
+
+	click = () => {
+		if (!this.props.gameState.tableClicked && !this.state.used && !this.props.readonly) {
+			var score = this.computeScore()
+			this.setState({used: true, score:score});
+			this.props.addScore(score);
+			this.props.handleScoreClick();
+		}	
+	}
+
+	cursorOn = () => {
+		if(this.props.gameState.roll > 0 && !this.props.gameState.tableClicked && !this.state.used && !this.props.readonly) {
+			this.setState({scoreDisplayed: this.computeScore()});
+		}
+	}
+
+	cursorOff = () => {
+		if(this.props.gameState.roll > 0 && !this.props.gameState.tableClicked && !this.state.used && !this.props.readonly) {
+			this.setState({scoreDisplayed: ""});
+		}
+	}
+
+	computeScore() {
+		var dice = [...this.props.gameState.numbers]
+		switch(this.props.name) {
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+				return this.sumOfFace(this.props.name, dice);
+
+			case "Brelan":
+				return (this.checkIfBrelan(dice) ? this.sumOfAllDice(dice) : 0);
+			case "Carré":
+				return (this.checkIfCarre(dice) ? this.sumOfAllDice(dice) : 0);
+			case "Full":
+				return (this.checkIfFull(dice) ? 25 : 0);
+			case "Petite suite":
+				return (this.checkIfPetiteSuite(dice) ? 30 : 0);
+			case "Grande suite":
+				return (this.checkIfGrandeSuite(dice) ? 40 : 0);
+			case "Yahtzee":
+				return (this.checkIfYahtzee(dice) ? 50 : 0);
+			case "Chance":
+				return this.sumOfAllDice(dice);
+			default:
+				return 0;
+		}
+
+	}
+
+	checkIfBrelan(dice) {
+		return Object.values(this.countFaces(dice)).some(n => n >= 3);
+	}
+	
+	checkIfCarre(dice) {
+		return Object.values(this.countFaces(dice)).some(n => n >= 4);
+	}
+
+	checkIfYahtzee(dice) {
+		return Object.values(this.countFaces(dice)).some(n => n >= 5);
+	}
+	
+	checkIfFull(dice) {
+		return JSON.stringify(Object.values(this.countFaces(dice)).sort()) === JSON.stringify([2,3]);
+	}
+
+	checkIfPetiteSuite(dice) {
+		var set = [...new Set(dice)];
+		set.sort();
+		if (set.length === 4) {
+			return (JSON.stringify(set) === JSON.stringify([1,2,3,4]) || JSON.stringify(set) === JSON.stringify([2,3,4,5]) || JSON.stringify(set) === JSON.stringify([3,4,5,6]));
+		} else if (set.length === 5) {
+			return (JSON.stringify(set) !== JSON.stringify([1,2,4,5,6]) && JSON.stringify(set) !== JSON.stringify([1,2,3,5,6]));
+		} else {
+			return false;
+		}
+	}
+
+	checkIfGrandeSuite(dice) {
+		return (JSON.stringify(dice.sort()) === JSON.stringify([1,2,3,4,5]) || JSON.stringify(dice.sort()) === JSON.stringify([2,3,4,5,6]));
+	}
+
+	sumOfFace(face, dice) {
+		// eslint-disable-next-line
+		var filtered = dice.filter(d => d == face);
+		return (filtered.length > 0 ? filtered.reduce((a,b) => a + b) : 0);
+	}
+
+	sumOfAllDice(dice) {
+		return dice.reduce((a,b) => a+b);
+	}
+
+	countFaces(dice) {
+		var counts = {};
+		for (var i=0; i < dice.length; i++) {
+			var num = dice[i];
+			counts[num] = counts[num] ? counts[num] + 1 : 1;
+		}
+		return counts;
+	}
+	
+	render() {
+		return (
+			<tr className={(this.props.readonly ? "readonly" : (this.state.used ? "used" : ""))} >
+				<td className="score-name">{this.props.name}</td>
+				<td className="score-desc">{this.props.desc}</td>
+				<td className="score-cell" onClick={this.click} onMouseOver={this.cursorOn} onMouseOut={this.cursorOff}>{this.state.scoreDisplayed}</td>
+			</tr>
+		)
+	}
+
+}
